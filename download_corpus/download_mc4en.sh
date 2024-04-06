de5262979236d18b09b376247c4f8344593c5405
viajiefan <viajiefan@gmail.com>
2024-04-06 16:19:07 +0900

    add concat, split command examples.

--- download_corpus/download_mc4en.sh
+++ download_corpus/download_mc4en.sh
@@ -1,4 +1,4 @@
-# Download MC4 (En)
+# Download MC4 (En) https://huggingface.co/datasets/allenai/c4
 cd ../data
 GIT_LFS_SKIP_SMUDGE=1 git clone https://huggingface.co/datasets/allenai/c4
 cd c4
@@ -7,4 +7,4 @@ time git lfs pull --include "en/*"
 # -> Download *.josn.gz
 
 # (Option) uncompress .gz (not keeping the original .gz)
-gunzip *.jsonl.gz
+gunzip *.json.gz
--- download_corpus/download_mc4ja.sh
+++ download_corpus/download_mc4ja.sh
@@ -1,10 +1,19 @@
-# Download MC4 (Ja)
+# Download MC4 (Ja) https://huggingface.co/datasets/allenai/c4
 cd ../data
 GIT_LFS_SKIP_SMUDGE=1 git clone https://huggingface.co/datasets/allenai/c4
 cd c4
 time git lfs pull --include "multilingual/c4-ja.*.json.gz"
 
-# -> Download *.josn.gz
+# ->  *.josn.gz
 
 # (Option) uncompress .gz (not keeping the original .gz)
-gunzip *.jsonl.gz
+# gunzip *.json.gz
+
+# (Option) concat
+# cat c4-ja*.jsonl > tmp/c4-ja.jsonl
+
+# (Option) split (job時間制限を考えて1file 5G以下に)
+# 行の途中で分割しない https://qiita.com/b-mente/items/0a57e65687d67b4ac582
+# N file(ここでは160)に分割する場合　出力ファイルベースファイル名+数字出力
+# split -n l/160 -d --additional-suffix=.jsonl c4-ja.jsonl c4-ja-
+# -> c4-ja-00, ...
